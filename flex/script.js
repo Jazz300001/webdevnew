43e466dcecc392ff87c96aa1229f5493b917b3d5
Jaskaran Chahal <Jschahal@uncg.edu>
2024-09-19 15:05:22 -0400

    wdec

--- flex/script.js
+++ flex/script.js
@@ -1,8 +1,17 @@
-const myDiv = document.querySelector("#div1"); 
-myDiv.addEventListener('click', addParagraph); 
-function addParagraph(event) { 
-    const myDiv = event.currentTarget; 
-    let para = document.createElement("p"); 
-    let node = document.createTextNode("This is new."); 
-    para.appendChild(node); 
-    myDiv.appendChild(para); }
+/** * journal.js */ 
+document.querySelector("button").addEventListener("click", addJournalEntry); 
+/** * TODO * adds a journal entry to the journal page */ 
+const myDiv = document.querySelector("#entries");
+myDiv.addEventListener('click', addJournalEntry);
+const date = document.getElementById('date');
+journaldate=date.value;
+const entry = document.getElementById('entry');
+journalentry=entry.value;
+function addJournalEntry(event) {
+    const myDiv = event.currentTarget;
+    let article = document.createElement('article');
+    article.id = journaldate;
+    article.textContent=journalentry;
+    myDiv.appendChild(article);
+
+}
